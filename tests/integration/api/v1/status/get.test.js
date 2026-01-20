59defefa7081ce0bb37bcaea2198836bb5ece1ab
Lucas de Freitas Vidigal <lucasvidigal3@gmail.com>
2026-01-20 00:50:21 +0000

    finish `/api/v1/status` enspoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const resposneBody = await response.json();
+  expect(resposneBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(resposneBody.updated_at).toISOString();
+  expect(resposneBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(resposneBody.dependencies.database.version).toEqual('16.10');
+  expect(resposneBody.dependencies.database.max_connections).toEqual(100);
+  expect(resposneBody.dependencies.database.opened_connections).toEqual(1);
 });
