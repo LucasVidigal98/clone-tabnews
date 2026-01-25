042a4ab867ffd840c7a40abf9ebcdc2b08459fb7
Lucas de Freitas Vidigal <lucasvidigal3@gmail.com>
2026-01-25 20:05:34 +0000

    add `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,17 @@
+import databse from 'infra/database';
+
+async function cleanDatabase() {
+  await databse.query('drop schema public cascade; create schema public');
+}
+
+beforeAll(cleanDatabase);
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
